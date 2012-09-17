21ca89613c0c1829d57db8b4058a4b3627e0bdf3
hoarf <alan.ficagna@gmail.com>
2012-09-16 23:48:57 -0300

    s

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 
 /* uso: ./etapa1 nome_do_arquivo */
 int main(int argc, char **argv) {
+	int token;
+	initMe();
+	token = yylex();
 	if (argc < 2) {
-		initMe();
-		token = yylex();
 		yyin= fopen(argv[1],"r");
 
 		while(running) {

7d9544750bc48fe483815bd81fc5dbe604248d9d
Marco Centeno <marcodurencenteno@yahoo.com.br>
2012-11-12 00:14:09 -0200

    Update astree.h

--- astree.h
+++ astree.h
@@ -63,7 +63,7 @@ ASTREE * astree_create_empty(int type);
 
 void astree_exibe(ASTREE* arvore, int level);
 
-void astree_exibe_botton_up(ASTREE* arvore, int level);
+void astree_exibe_bottom_up(ASTREE* arvore, int level);
 
 void astree_toFile(ASTREE* arvore,const char* filenName);
 
